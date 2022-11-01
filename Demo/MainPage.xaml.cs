e1745204b0ae9f794b2a06121ad06eec98cfad33
zZBrigadierZz <danfiala23@gmail.com>
2022-11-01 14:03:28 +0100

    Initial commit.

---
+++ Demo/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Maui.UniversalLayouts/Layout/UniversalHorizontalStackLayout.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Layouts;
+
+namespace Maui.UniversalLayouts.Layout;
+
+/// <summary>
+/// This is a custom layout that is used to display a list of items in horizontal orientation.
+/// And respects the their alignment. Based on <see cref="HorizontalStackLayout"/>.
+/// </summary>
+[ContentProperty(nameof(Children))]
+public class UniversalHorizontalStackLayout : StackBase
+{
+    protected override ILayoutManager CreateLayoutManager() => new UniversalHorizontalStackLayoutManager(this);
+}
